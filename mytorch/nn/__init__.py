9d4ca22d2b2bec32ae408bedecc8d9b2740475df
Edward Yao <syao89@gatech.edu>
2026-05-21 23:13:22 -0400

    update mytorch/nn/init

--- mytorch/nn/__init__.py
+++ mytorch/nn/__init__.py
@@ -2,3 +2,8 @@ from .activation import Identity, Sigmoid, Tanh, ReLU, GELU, Softmax
 from .batchnorm import BatchNorm1d
 from .linear import Linear
 from .loss import MSELoss, CrossEntropyLoss
+from .Conv1d import Conv1d_stride1, Conv1d
+from .Conv2d import Conv2d_stride1, Conv2d
+from .ConvTranspose import ConvTranspose1d, ConvTranspose2d
+from .pool import MaxPool2d_stride1, MaxPool2d, MeanPool2d_stride1, MeanPool2d
+from .resampling import Upsample1d, Upsample2d, Downsample1d, Downsample2d
