856653b36eb8ba1f7f27e04b77d74988577f9a61
Edward Yao <syao89@gatech.edu>
2026-05-21 22:58:32 -0400

    Add files via upload

--- models/mlp_scan.py
+++ models/mlp_scan.py
@@ -127,3 +127,33 @@ class CNN_DistributedScanningMLP():
         for layer in self.layers[::-1]:
             dLdA = layer.backward(dLdA)
         return dLdA
+    
+class MLP():
+
+    def __init__(self, layer_sizes):
+
+        self.layers = []
+        for i in range(len(layer_sizes) - 1):
+            in_size, out_size = layer_sizes[i], layer_sizes[i + 1]
+            self.layers.append(Linear(in_size, out_size))
+            self.layers.append(ReLU())
+        self.layers = self.layers[:-1]
+
+    def init_weights(self, weights):
+
+        for i in range(len(weights)):
+            self.layers[i * 2].W = weights[i].T
+
+    def forward(self, A):
+
+        Z = A
+        for layer in self.layers:
+            Z = layer(Z)
+        return Z
+
+    def backward(self, dLdZ):
+        
+        dLdA = dLdZ
+        for layer in self.layers[::-1]:
+            dLdA = layer.backward(dLdA)
+        return dLdA
