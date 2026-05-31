f5567fcb82dc66a50de979a3fe8b837bdc028580
Edward Yao <syao89@gatech.edu>
2026-05-30 21:34:49 -0400

    update models/init

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .mlp import MLP0, MLP1, MLP4
 from .cnn import CNN
 from .mlp_scan import CNN_SimpleScanningMLP, CNN_DistributedScanningMLP, MLP
+from .CTCDecoding import GreedySearchDecoder, BeamSearchDecoder
